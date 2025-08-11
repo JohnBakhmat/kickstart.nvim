2801541b1c773f6dd079fadd057c7e86d13486d5
John Bakhmat <johnbakhmat@gmail.com>
2025-08-11 17:13:47 +0300

    fix prettier in react typesript

--- init.lua
+++ init.lua
@@ -524,7 +524,7 @@ require('lazy').setup({
 
           -- Opens a popup that displays documentation about the word under your cursor
           --  See `:help K` for why this keymap.
-          map('K', require('hover').hover, 'Hover Documentation')
+          map('K', require('noice.lsp').hover, 'Hover Documentation')
 
           -- WARN: This is not Goto Definition, this is Goto Declaration.
           --  For example, in C this would take you to the header.
@@ -582,11 +582,6 @@ require('lazy').setup({
         gleam = {
           filetypes = {
             'gleam',
-          },
-        },
-        volar = {
-          filetypes = {
-            'vue',
           },
         },
         lua_ls = {
@@ -665,15 +660,31 @@ require('lazy').setup({
           lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
         }
       end,
+
       formatters_by_ft = {
         lua = { 'stylua' },
         javascript = { 'prettierd', 'prettier', stop_after_first = true },
         typescript = { 'prettierd', 'prettier', stop_after_first = true },
+        typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
         json = { 'prettierd', 'prettier', stop_after_first = true },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
       },
     },
+
+    init = function()
+      vim.api.nvim_create_user_command('Format', function(args)
+        local range = nil
+        if args.count ~= -1 then
+          local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+          range = {
+            start = { args.line1, 0 },
+            ['end'] = { args.line2, end_line:len() },
+          }
+        end
+        require('conform').format { async = true, lsp_format = 'fallback', range = range }
+      end, { range = true })
+    end,
   },
 
   { -- Autocompletion
